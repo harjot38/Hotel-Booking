0250bf8bed20b4c5bf8f8470980edecf85612525
harjot38 <144142200+harjot38@users.noreply.github.com>
2025-05-29 22:43:53 -0600

    Added FeaturedDestination cards.

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import Hero from '../components/Hero'
-import HotelCard from '../components/HotelCard'
+import FeaturedDestination from '../components/FeaturedDestination'
 
 const Home = () => {
     return (
         <>
             <Hero />
-            {/* <HotelCard /> */}
+            <FeaturedDestination />
+            
         </>
     )
 }
