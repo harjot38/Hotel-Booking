a12b2c6612695e7f405d77d0e3997f80325f5611
harjot38 <144142200+harjot38@users.noreply.github.com>
2025-06-16 22:13:20 -0600

    started working on backend.

--- server/server.js
+++ server/server.js
@@ -9,3 +9,5 @@ app.use(cors())
 app.get('/', (req, res) => res.send("API is working"))
 
 const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
