ee51e6eb2f0f846fdaba1b006bd62d60a5afb3b8
harjot38 <144142200+harjot38@users.noreply.github.com>
2025-06-22 20:43:32 -0600

    added api/user

---
+++ server/routes/userRoutes.js
@@ -0,0 +1,9 @@
+import express from "express";
+import { getUserData } from "../controllers/userController.js";
+import { protect } from "../middleware/authMiddleware.js";
+
+const userRouter = express.Router();
+
+userRouter.get('/', protect, getUserData);
+
+export default userRouter;
